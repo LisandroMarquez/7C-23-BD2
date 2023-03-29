7d7bdff21c9ca489b97432dfe89ff04f898b3ffa
Lichi <lichimarquez04@gmail.com>
2023-03-29 11:02:52 -0300

    more minor changes

--- Clases/04/Querrys.sql
+++ Clases/04/Querrys.sql
@@ -1,49 +1,49 @@
-/* QUERRYS */
+-- QUERRYS
 
-/* 1 */
+-- 1
 
 Select f.title, f.special_features
 From film f
 Where f.rating="PG-13";
 
-/* 2 */
+-- 2
 
 Select distinct f.length
 From film f
 Order by f.length;
 
-/* 3 */
+-- 3
 
 Select f.title, f.rental_rate, f.replacement_cost
 From film f
 Where f.replacement_cost = 24;
 
-/* 4 */
+-- 4
 
 Select f.title, c.name, f.rating
 From film f Join film_category fc on f.id = fc.film_id Join category c on fc.category_id = c.id
 Where f.special_features = "Behing the Scenes";
 
-/* 5 */
+-- 5
 
 Select a.first_name, a.last_name
 From actor a Join film_actor fa on a.id = fa.actor_id Join film f on fa.film_id = f.id
 Where f.title = 'ZOOLANDER FICTION';
 
-/* 6 */
+-- 6
 
 Select a.address, ci.city, co.country
 From store s Join address a on s.address_id = a.id Join city ci on a.city_id = ci.id Join country co on ci.country_id = co.id
 Where s.id = 1;
 
-/* 7 */
+-- 7
 
 Select f.title, f.rating
 From film f
 Group by f.rating
 Order by f.rating, f.title;
 
-/* 8 */
+-- 8
 
 Select f.title, concat(st.first_name, ' ', st.last_name) as full_name
 From film f Join inventory i on f.id = i.film_id Join store s on i.store_id = s.id Join staff st on s.id = st.store_id
--- Clases/06/subquerry.sql
+++ Clases/06/subquerry.sql
@@ -1,4 +1,6 @@
-/* Ejercicio 1 */
+-- SUBQUERRIES
+
+-- Ejercicio 1
 
 SELECT a1.first_name, a1.last_name
 FROM actor a1
@@ -8,7 +10,7 @@ WHERE EXISTS    (SELECT *
                 AND a1.id <> a2.id)
 ORDER BY a1.first_name;
 
-/* Ejercicio 2 */
+-- Ejercicio 2
 
 SELECT concat(a.first_name, ' ', a.last_name) as full_name
 FROM actor a
@@ -17,7 +19,7 @@ WHERE NOT EXISTS    (SELECT *
                     WHERE a.id = fm.actor_id
 );
 
-/* Ejercicio 3 */
+-- Ejercicio 3
 
 SELECT concat(c.first_name, ' ', c.last_name) as full_name
 FROM customer c
@@ -26,7 +28,7 @@ WHERE NOT EXISTS    (SELECT *
                     WHERE c.id = r.customer_id
 );
 
-/* Ejercicio 4 */
+-- Ejercicio 4
 
 SELECT concat(c.first_name, ' ', c.last_name) as full_name
 FROM customer c
@@ -35,45 +37,45 @@ WHERE          (SELECT count(*)
                 WHERE c.id = r.customer_id
 ) > 1;
 
-/* Ejercicio 5 */
+-- Ejercicio 5
 
 SELECT concat(a.first_name, ' ', a.last_name) as full_name
 FROM actor a
 WHERE EXISTS    (SELECT *
-                FROM film f JOIN film_actor fm on f.id = fm.film_id3
+                FROM film f JOIN film_actor fm on f.id = fm.film_id
                 WHERE f.id = fm.film_id
                 AND a.id = fm.actor_id
                 AND (f.title = 'BETRAYED REAR' or f.title = 'CATCH AMISTAD')
 );
 
-/* Ejercicio 6 */
+-- Ejercicio 6
 
 SELECT concat(a.first_name, ' ', a.last_name) as full_name
 FROM actor a
 WHERE EXISTS    (SELECT *
-                FROM film f JOIN film_actor fm on f.id = fm.film_id3
+                FROM film f JOIN film_actor fm on f.id = fm.film_id
                 WHERE f.id = fm.film_id
                 AND a.id = fm.actor_id
                 AND (f.title = 'BETRAYED REAR' AND NOT f.title = 'CATCH AMISTAD')
 );
 
-/* Ejercicio 7 */
+-- Ejercicio 7
 
 SELECT concat(a.first_name, ' ', a.last_name) as full_name
 FROM actor a
 WHERE EXISTS    (SELECT *
-                FROM film f JOIN film_actor fm on f.id = fm.film_id3
+                FROM film f JOIN film_actor fm on f.id = fm.film_id
                 WHERE f.id = fm.film_id
                 AND a.id = fm.actor_id
                 AND (f.title = 'BETRAYED REAR' AND f.title = 'CATCH AMISTAD')
 );
 
-/* Ejercicio 8 */
+-- Ejercicio 8
 
 SELECT concat(a.first_name, ' ', a.last_name) as full_name
 FROM actor a
 WHERE NOT EXISTS    (SELECT *
-                    FROM film f JOIN film_actor fm on f.id = fm.film_id3
+                    FROM film f JOIN film_actor fm on f.id = fm.film_id
                     WHERE f.id = fm.film_id
                     AND a.id = fm.actor_id
                     AND (f.title = 'BETRAYED REAR' or f.title = 'CATCH AMISTAD')
