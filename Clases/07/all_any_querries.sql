7f771e00a694ba384297027513e0cec5d43e7402
Lichi <lichimarquez04@gmail.com>
2023-04-14 11:47:55 -0300

    All Any Querries almost done

--- Clases/07/all_any_querries.sql
+++ Clases/07/all_any_querries.sql
@@ -1,33 +1,41 @@
 -- All & Any Querries
+use sakila;
+
 -- (1)
-Select title, rating
-From film
-Where length <= All     (Select length
-                        From film);
+SELECT title, rating, length as min_length
+FROM film f1
+WHERE length <= All (SELECT length
+                    FROM film f2);
 
 -- (2)
-Select title
-From film f1
-Where length <= All     (Select length
-                        From film f2)
-Having count(*) = 1;
+SELECT title
+FROM film
+WHERE length <= ALL
+    (SELECT length
+    FROM film)
+HAVING COUNT(*) = 1;
 
 -- (3)
 Select CONCAT(c.first_name, ' ', c.last_name) as full_name, a.address, p.amount
 From customer c
-Join payment p on c.id = p.customer_id
-Join address a on c.address_id = a.id
+Join payment p on c.customer_id = p.customer_id
+Join address a on c.address_id = a.address_id
 Where p.amount <= All   (Select p2.amount
-                        From payment p2);
+                        From payment p2)
+Order by c.first_name;
 
 -- (4)
--- PREGUNTAR COMO DEVOLVER ESTO EN LA MISMA FILA !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-Select CONCAT(c.first_name, ' ', c.last_name) as full_name, a.address, p.amount
+-- Mostrar misma fila, no columna :/
+Select CONCAT(c.first_name, ' ', c.last_name) as full_name, a.address, MIN(p.amount) as amount
 From customer c
-Join payment p on c.id = p.customer_id
-Join address a on c.address_id = a.id
+Join payment p on c.customer_id = p.customer_id
+Join address a on c.address_id = a.address_id
 Where p.amount <= All   (Select p2.amount
                         From payment p2)
-or p.amount >= All   (Select p2.amount
-                        From payment p2)
-Order by p.amount;
+UNION
+Select CONCAT(c.first_name, ' ', c.last_name) as full_name, a.address, MAX(p.amount) as amount
+From customer c
+Join payment p on c.customer_id = p.customer_id
+Join address a on c.address_id = a.address_id
+Where p.amount >= All   (Select p2.amount
+                        From payment p2);
